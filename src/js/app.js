fad2aa604ac88cdbf6916e0d3f5f2962f2c0c743
Sergio MT <sergio.mt88@gmail.com>
2017-11-10 17:56:40 +0100

    Script build:watch repaired and Angular basic structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,6 @@
 const angular = require('angular')
+const MainController = require('./controllers/MainController.js')
+// const ApiService = require('./services/ApiService.js')
+
+angular.module('dvdApp', [])
+	.controller('MainController', MainController)
---
+++ src/js/controllers/MainController.js
@@ -0,0 +1,5 @@
+function MainController($scope) {
+
+}
+
+module.exports = MainController
