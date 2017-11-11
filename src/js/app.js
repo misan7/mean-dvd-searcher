0f44f1fd0957d455433fbdd52f28bc4e869848bd
Sergio MT <sergio.mt88@gmail.com>
2017-11-11 10:34:10 +0100

    Home template using <ng-view> and Angular-route

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 const angular = require('angular')
+const angularRoute = require('angular-route')
+
 const MainController = require('./controllers/MainController.js')
 const ApiService = require('./services/ApiService.js')
 
-angular.module('dvdApp', [])
+angular.module('dvdApp', [angularRoute])
 	.controller('MainController', MainController)
 	.factory('ApiService', ApiService)
+	.config($routeProvider => {
+
+		$routeProvider
+			.when('/', {
+				templateUrl: 'templates/home.html',
+				controller: 'MainController'
+			})
+	})
